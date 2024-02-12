f69c0ec7f4d2d19c8f53a08f96ebafc30529332d
Hernan <hacc.code@gmail.com>
2024-02-12 20:23:06 -0300

    Handlebars + Utils

--- app.js
+++ app.js
@@ -2,3 +2,25 @@ import express from "express"
 import { engine } from "express-handlebars"
 import mongoose from "mongoose"
 
+
+
+const app = express()
+
+
+app.engine('handlebars', engine())
+app.set('view engine','hanldebars')
+app set('views', './views')
+
+
+app.use(express.static('public'))
+
+
+
+app.use(express.json())
+app.use(express.urlencoded({ extended:true }))
+
+const server = app.listen(8080, () =>
+console.log('Server running in port 8080')
+)
+
+
